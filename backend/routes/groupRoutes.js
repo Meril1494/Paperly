e9274bf091bc7c5e15cdc8ec5ab44fce146ae14e
Meril1494 <163733051+Meril1494@users.noreply.github.com>
2025-08-12 15:12:03 +0530

    chore: clean group routes

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -6,8 +6,6 @@ const Classroom = require('../models/Classroom');
 const auth = require('../middleware/auth');
 
 // Create a new group
-// Create a new group
-// Create a new group
 router.post('/create', auth, async (req, res) => {
   console.log('Received request to create group:', req.body);
 
@@ -44,36 +42,6 @@ router.post('/create', auth, async (req, res) => {
     return res.status(500).json({ message: 'Error creating group' });
   }
 });
-
-// router.post('/create', auth, async (req, res) => {
-//   console.log('Received request to create group:', req.body); // ✅ Debugging log
-
-//   const { classroomId, name, description, isPrivate, groupType } = req.body;
-//   let joinCode = null;
-
-//   try {
-//     if (isPrivate) {
-//       joinCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-//     }
-
-//     const group = new Group({
-//       classroomId,
-//       name: name,
-//       description,
-//       isPrivate,
-//       joinCode,
-//       groupType,
-//       createdBy: req.user._id,
-//       members: [req.user._id]
-//     });
-
-//     await group.save();
-//     res.status(201).json({ message: 'Group created successfully', group });
-//   } catch (err) {
-//     console.error('Group creation error:', err);
-//     res.status(500).json({ message: 'Error creating group' });
-//   }
-// });
 
 // Join a group (with or without code)
 router.post('/join', auth, async (req, res) => {
